50cb12cc7a5b6669ad2a0eeb5e562993a94f1fbb
Md Hosam Nayeem <85168018+MHNayeem25@users.noreply.github.com>
2025-03-07 14:12:45 +0530

    Add files via upload

---
+++ Day-2/TemplateEngine.cs
@@ -0,0 +1,20 @@
+﻿namespace TemplateEngine;
+
+public class TemplateEngine
+{
+    Dictionary<string, string> keyValuePairs = new Dictionary<string, string>();
+    public string Evaluate()
+    {
+        return "Hello NM";
+    }
+
+    public void setTemplate(string template)
+    {
+
+    }
+
+    public void setVariables(string v1, string v2)
+    {
+
+    }
+}
